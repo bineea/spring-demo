e77c0c474cad3a044638da15e375e0f92d045adf
bineea <33547731+bineea@users.noreply.github.com>
2021-07-04 16:43:40 +0800

    实例化bean的方式

---
+++ src/main/java/my/demo/manager/xml/CreateHelloworldManagerImpl.java
@@ -0,0 +1,8 @@
+package my.demo.manager.xml;
+
+public class CreateHelloworldManagerImpl {
+
+    public HelloworldManagerImpl getHelloworldManagerImplInstance() {
+        return new HelloworldManagerImpl();
+    }
+}
---
+++ src/main/java/my/demo/manager/xml/HelloworldManagerImplFacory.java
@@ -0,0 +1,8 @@
+package my.demo.manager.xml;
+
+public class HelloworldManagerImplFacory {
+
+    public static HelloworldManagerImpl getHelloworldManagerImplInstance() {
+        return new HelloworldManagerImpl();
+    }
+}
