ae16a6569c54742c615c022f52840a4cc966fe92
bineea <33547731+bineea@users.noreply.github.com>
2021-12-30 17:42:24 +0800

    @Bean创建bean对象方法增加参数，spring自动注入

--- spring-ano-demo/src/main/java/my/demo/config/AppConfig.java
+++ spring-ano-demo/src/main/java/my/demo/config/AppConfig.java
@@ -1,7 +1,6 @@
 package my.demo.config;
 
 import my.demo.manager.CreateHelloworldManager;
-import my.demo.manager.HelloworldManagerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
