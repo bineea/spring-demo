3cf497a70f2a744dbfaaeaa6493702d045fccdf2
bineea <33547731+bineea@users.noreply.github.com>
2022-01-20 23:40:57 +0800

    调整web

--- spring-xml-demo/src/main/java/my/demo/servlet/TransferServlet.java
+++ spring-xml-demo/src/main/java/my/demo/servlet/TransferServlet.java
@@ -21,7 +21,7 @@ public class TransferServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doGet(req, resp);
+        super.doPost(req, resp);
     }
 
     @Override
