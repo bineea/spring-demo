2c5bf0f15d74014cb0b20eb3b5c659676a21ef4c
bineea <33547731+bineea@users.noreply.github.com>
2022-01-21 23:26:50 +0800

    测试DispatcherServlet导致spring重复启动

--- spring-ano-xml-demo/src/main/java/my/demo/service/TransferServiceImpl.java
+++ spring-ano-xml-demo/src/main/java/my/demo/service/TransferServiceImpl.java
@@ -3,8 +3,10 @@ package my.demo.service;
 import my.demo.dao.entity.SampleTransfer;
 import my.demo.dao.mapper.SampleTransferMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.PostConstruct;
 import java.math.BigDecimal;
 
 @Service
@@ -12,6 +14,14 @@ public class TransferServiceImpl  implements TransferService{
 
     @Autowired
     private SampleTransferMapper sampleTransferMapper;
+
+    @Value("${app.name}")
+    private String appName;
+
+    @PostConstruct
+    public void init() {
+        System.out.println("初始化应用："+appName+"-"+"my.demo.service.TransferServiceImpl~~~");
+    }
 
     @Override
     public SampleTransfer findByAccount(String account) {
